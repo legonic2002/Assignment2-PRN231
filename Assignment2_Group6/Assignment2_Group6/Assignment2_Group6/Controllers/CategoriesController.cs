930de9c993286e5657dada0e12124df32eb9f1d3
legonic2002 <93648882+legonic2002@users.noreply.github.com>
2023-10-15 10:24:54 +0700

    aa
    sa

---
+++ Assignment2_Group6/Assignment2_Group6/Assignment2_Group6/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Assignment2_Group6.Models;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace Assignment2_Group6.Controllers
+{
+    
+    public class CategoriesController : ODataController
+    {
+        private readonly eStoreContext _context;
+
+        public CategoriesController(eStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categories
+        [HttpGet]
+        [EnableQuery]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+          if (_context.Categories == null)
+          {
+              return NotFound();
+          }
+            return await _context.Categories.ToListAsync();
+        }
+
+        // GET: api/Categories/5
+        [HttpGet("odata/Categories/{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+          if (_context.Categories == null)
+          {
+              return NotFound();
+          }
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+       
+    }
+}
